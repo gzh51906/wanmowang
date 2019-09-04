e3ddda9fda71011ad41e7f1e0c99785d5950d44b
陈日兴 <2367800442@qq.com>
2019-09-04 20:37:11 +0800

    后台管理组件测试

--- manage/src/App.vue
+++ manage/src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <home v-if="usercheck"></home>
+    <login v-else></login>
   </div>
 </template>
 
 <script>
+import home from "./pages/home.vue";
 export default {
   name: "app",
-  components: {}
+  data() {
+    usercheck: false;
+  },
+  components: {
+    home
+  },
+  created() {
+    let usercheck = localStorage.getItem("usercheck");
+    if (usercheck) {
+      this.usercheck = true;
+    }
+  }
 };
 </script>
 
 <style>
+#app {
+}
 </style>
