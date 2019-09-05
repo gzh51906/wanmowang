b79b982ba921125d8c194ffcb74f024118597c4f
陈日兴 <2367800442@qq.com>
2019-09-05 10:57:51 +0800

    完成管理首页部分功能

--- manage/src/pages/home.vue
+++ manage/src/pages/home.vue
@@ -4,38 +4,68 @@
       <h1>玩墨网后台管理系统</h1>
       <el-collapse v-model="activeName" accordion class="overflow">
         <el-collapse-item title="商品管理" name="1">
-          <div>商品列表</div>
-          <div>商品分类</div>
+          <div @click="change('/商品管理/商品列表')">商品列表</div>
+          <div @click="change('/商品管理/商品分类')">商品分类</div>
         </el-collapse-item>
         <el-collapse-item title="用户管理" name="2">
-          <div>用户列表</div>
-          <div>添加用户</div>
-          <div>管理人员</div>
+          <div @click="change('/用户管理/用户列表')">用户列表</div>
+          <div @click="change('/用户管理/添加用户')">添加用户</div>
+          <div @click="change('/用户管理/管理人员')">管理人员</div>
         </el-collapse-item>
         <el-collapse-item title="订单管理" name="3">
-          <div>订单列表</div>
+          <div @click="change('/订单管理/订单列表')">订单列表</div>
         </el-collapse-item>
       </el-collapse>
     </el-aside>
     <el-main>
-      <div class="topHead"></div>
+      <div class="topHead">
+        <span>{{$store.state.common.title}}</span>
+        <span class="tuichu" @click="tuichu">退出</span>
+      </div>
       <router-view></router-view>
     </el-main>
   </el-container>
 </template>
 <script>
+import { mapMutations } from "vuex";
 export default {
   data() {
     return {
       activeName: "1"
     };
+  },
+  methods: {
+    change(title) {
+      this.$store.commit("updateTitle", "首页" + title);
+    },
+    ...mapMutations({
+      tuichu: "removeUser"
+    })
   }
 };
 </script>
 <style scoped>
+.el-collapse div {
+  cursor: pointer;
+}
 .topHead {
   height: 52px;
   border-bottom: 2px solid rgb(242, 242, 242);
+}
+.topHead > span {
+  font-size: 14px;
+  color: #666;
+  float: left;
+  line-height: 54px;
+  margin-left: 20px;
+}
+.topHead .tuichu {
+  float: right;
+  margin-right: 30px;
+  color: blue;
+  cursor: pointer;
+  line-height: 18px;
+  margin-top: 17px;
 }
 .el-collapse div {
   text-align: center;
--- manage/src/store/common.js
+++ manage/src/store/common.js
@@ -2,7 +2,8 @@ export default {
     // 请写好注释，下面依次类推
     state: {
         // 陈日兴定义的
-        userCheck: false
+        userCheck: false,
+        title: "/首页/商品管理/商品分类"
         // 黄日隆定义的
 
         // 苏沛龙定义的
@@ -13,9 +14,21 @@ export default {
     },
     mutations: {
         //注释
-        userChange(state,username) {
+        userChange(state, username) {
             localStorage.setItem("userCheck", username);
             state.userCheck = true;
+        },
+        update(state) {
+            if (localStorage.getItem("userCheck")) {
+                state.userCheck = true;
+            }
+        },
+        updateTitle(state, data) {
+            state.title = data;
+        },
+        removeUser(state) {
+            state.userCheck = false;
+            localStorage.removeItem("userCheck");
         }
     },
     actions: {
