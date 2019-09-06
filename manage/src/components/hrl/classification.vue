48955fd64f4be8db52c51556ab23c01abfb7a2e8
黄日隆 <l>
2019-09-06 21:20:34 +0800

    完成添加、修改商品类型

--- manage/src/components/hrl/classification.vue
+++ manage/src/components/hrl/classification.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <el-button slot="reference" type="primary" icon="el-icon-circle-plus" @click="Haddclass('addclass')">添加</el-button>
+    <el-button
+      slot="reference"
+      type="primary"
+      icon="el-icon-circle-plus"
+      @click="Haddclass('addclass')"
+    >添加</el-button>
     <el-popover placement="top" width="160" v-model="visible">
       <p>确定删除吗？</p>
       <div style="text-align: right; margin: 0">
         <el-button size="mini" type="text" @click="visible = false">取消</el-button>
         <el-button type="primary" size="mini" @click="visible = false">确定</el-button>
       </div>
-        <el-button slot="reference" type="danger" icon="el-icon-delete">删除</el-button>
+      <el-button slot="reference" type="danger" icon="el-icon-delete">删除</el-button>
     </el-popover>
 
     <el-table
@@ -38,20 +43,20 @@
       <el-table-column prop="num" label="数量" sortable show-overflow-tooltip>
         <template slot-scope="scope">{{ scope.row.num }}</template>
       </el-table-column>
-       <el-table-column label="时间" sortable show-overflow-tooltip>
-        <template>{{ time }}</template>
-      </el-table-column>
+      <el-table-column prop="time" label="时间" sortable show-overflow-tooltip></el-table-column>
       <el-table-column label="操作">
+        <template slot-scope="scope">
         <el-button-group>
           <el-button
             size="small"
             type="primary"
             icon="el-icon-edit"
             round
-            @click="Haddtype('addtype')"
+            @click="Haddtype('addtype',scope.row._id)"
           >修改</el-button>
           <el-button size="small" type="danger" icon="el-icon-delete" round>删除</el-button>
         </el-button-group>
+        </template>
       </el-table-column>
     </el-table>
   </div>
@@ -61,106 +66,21 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          name: "服饰",
-          num: ""
-        },
-        {
-          name: "搭配",
-          num: ""
-        },
-        {
-          name: "数码",
-          num: ""
-        },
-        {
-          name: "餐具",
-          num: ""
-        },
-        {
-          name: " 出行",
-          num: ""
-        },
-        {
-          name: " 文具",
-          num: ""
-        },
-        {
-          name: "居家",
-          num: ""
-        },
-        {
-          name: "品牌",
-          num: ""
-        }
-      ],
+      tableData: [],
       multipleSelection: [],
       clothTpye: [],
       typeNum: "",
       visible: false,
-      time:'2019-11-11'
+      newdata: "",
+      newlis:[],
     };
   },
   async created() {
-    //请求服饰的数据
     let {
       data: { data }
-    } = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: "服饰"
-      }
-    });
-    this.tableData[0].num = data.length;
-    //请求搭配的数据
-    let dpData = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: "搭配"
-      }
-    });
-    this.tableData[1].num = dpData.data.data.length;
-    //请求数码的数据
-    let smData = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: "数码"
-      }
-    });
-    this.tableData[2].num = smData.data.data.length;
-    //请求餐厨的数据
-    let ccData = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: "餐具"
-      }
-    });
-    this.tableData[3].num = ccData.data.data.length;
-    //请求出行的数据
-    let cxData = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: " 出行"
-      }
-    });
-    this.tableData[4].num = cxData.data.data.length;
-    //请求文具的数据
-    let wjData = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: " 文具"
-      }
-    });
-    this.tableData[5].num = wjData.data.data.length;
-    //请求居家的数据
-    let jjData = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: "居家"
-      }
-    });
-    this.tableData[6].num = jjData.data.data.length;
-    //请求品牌的数据
-    let ppData = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
-      params: {
-        type: "品牌"
-      }
-    });
-    this.tableData[7].num = ppData.data.data.length;
+    } = await this.$axios.get("http://127.0.0.1:1901/hrl/classtype");
+    this.tableData = data;
+    console.log(this.$route.query);
   },
   methods: {
     handleSelectionChange(val) {
@@ -174,11 +94,10 @@ export default {
           title: command
         }
       });
-      console.log(data);
       this.typeNum = data.length;
     },
     async hTypelis(type) {
-      console.log(type);
+      // console.log(type);
       let {
         data: { data }
       } = await this.$axios.get("http://127.0.0.1:1901/hrl/class", {
@@ -192,8 +111,8 @@ export default {
     Haddclass(path) {
       this.$router.push(path);
     },
-    Haddtype(path) {
-      this.$router.push(path);
+    Haddtype(path,id) {
+      this.$router.push({path,query:{id:id}})
     }
   }
 };
