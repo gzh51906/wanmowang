a6a5945166a4ef588a36b5b5d53a2aecf72f7a40
supeilong <1072397843@qq.com>
2019-09-06 15:41:05 +0800

    完成商品列表页面

--- manage/src/store/Lgoodsdata.js
+++ manage/src/store/Lgoodsdata.js
@@ -10,10 +10,22 @@ export default {
             state.data = data
         },
         removeItem(state, goodsmsg) {
+
             state.data = state.data.filter(item => {
                 return goodsmsg.goods_id != item._id
 
             })
+        },
+        removeMoreItem(state, goodsmsg) {
+            let goods_id = goodsmsg.map(ele => {
+                return ele.goods_id
+            });
+            let index = goods_id.length
+            for (let i = 0; i < index; i++) {
+                state.data = state.data.filter((item) => {
+                    return goods_id[i] != item._id
+                })
+            }
         }
     }
 }
